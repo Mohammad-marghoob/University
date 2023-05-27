9c900634f83ad85b61d807528cf0f35e17299cc3
Mohamad Aria <109274828+Mohammad-marghoob@users.noreply.github.com>
2023-05-27 07:48:34 +0330

    multi three
    multiply a number entered by user

---
+++ multi_three.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace multiThree
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        int num;
+        Console.WriteLine("please enter a number");    
+        string input = console.ReadLine();
+        num = int.Parse(input)
+        num *= 3;
+        console.WriteLine(num);
+        console.ReadKey();
+    }
+  }
+}
