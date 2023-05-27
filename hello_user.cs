efdb18b8bbb365111679a5b28ee623988e2e2aa5
Mohamad Aria <109274828+Mohammad-marghoob@users.noreply.github.com>
2023-05-27 07:44:39 +0330

    username
    say hello to user after he/she enter their name

---
+++ hello_user.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace HelloUser
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        string userName;
+        Console.WriteLine("please enter your username");    
+        name = console.ReadLine();
+        console.WriteLine("hello " + userName);
+        console.WriteLine("Enter a key to exit");
+        console.ReadKey();
+    }
+  }
+}
