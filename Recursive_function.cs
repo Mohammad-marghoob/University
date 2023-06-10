88571064a3d8fd56b79bfa06bde11c36b9056334
Mohamad Aria <109274828+Mohammad-marghoob@users.noreply.github.com>
2023-06-10 08:34:54 +0330

    Recursive function
    Recursive function for fibonacci sequence

---
+++ Recursive_function.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static int Fibo(int input)
+        {
+            if (input <= 2)
+            {
+                return 1;
+            }
+            else
+            {
+                int sum = 0;
+
+                for (int i=2; i < input; i++)
+                {
+                    sum = Fibo(input - 1) + Fibo(input - 2);
+                }
+                return sum;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int num , result;
+            Console.WriteLine("please enter a number");
+            string input = Console.ReadLine();
+            num = int.Parse(input);
+            result = Fibo(num);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
